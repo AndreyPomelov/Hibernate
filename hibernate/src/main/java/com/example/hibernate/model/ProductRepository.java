a7094dcfd5e0fa5abf381191f23b4c45f4adfc12
Andrey <k-udm@ya.ru>
2021-06-09 17:02:50 +0400

    model files added

---
+++ hibernate/src/main/java/com/example/hibernate/model/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.example.hibernate.model;
+
+import java.util.List;
+
+public class ProductRepository {
+
+    public Product findById(Long id) {
+        return null;
+    }
+
+    public List<Product> findAll() {
+        return null;
+    }
+
+    public void deleteById(Long id) {
+
+    }
+
+    public Product saveOrUpdate(Product product) {
+        return null;
+    }
+}
